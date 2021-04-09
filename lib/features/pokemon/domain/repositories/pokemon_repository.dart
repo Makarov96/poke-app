bd3ae591163027909aa0f127dea1f8ac1dc3950d
scolocho <scolocho@popoyan.com>
2021-04-08 18:45:07 -0600

    add model and abstract repository

---
+++ lib/features/pokemon/domain/repositories/pokemon_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:poke_api_app/core/error/failure.dart';
+import 'package:poke_api_app/features/pokemon/data/models/pokemon_model.dart';
+
+abstract class PokemonRepostiry {
+  Future<Either<Failure, List<Results>>> getPokemonFromAPI();
+}
