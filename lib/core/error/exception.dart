3aa144ea40533b048a0b06d0d6d893603954b9cf
scolocho <scolocho@popoyan.com>
2021-04-08 18:57:47 -0600

    modify exxception and repository

--- lib/core/error/exception.dart
+++ lib/core/error/exception.dart
@@ -1,1 +1,34 @@
-class ServerException implements Exception {}
+import 'package:equatable/equatable.dart';
+
+class ServerException implements Exception, Equatable {
+  final _message;
+  final _prefix;
+
+  ServerException([this._message, this._prefix]);
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+
+  String toString() {
+    return "$_prefix$_message";
+  }
+}
+
+class FetchDataException extends ServerException {
+  FetchDataException([String message])
+      : super(message, "Error During Communication: ");
+}
+
+class BadRequestException extends ServerException {
+  BadRequestException([message]) : super(message, "Invalid Request: ");
+}
+
+class UnauthorisedException extends ServerException {
+  UnauthorisedException([message]) : super(message, "Unauthorised: ");
+}
+
+class InvalidInputException extends ServerException {
+  InvalidInputException([String message]) : super(message, "Invalid Input: ");
+}
--- lib/features/pokemon/domain/repositories/pokemon_repository.dart
+++ lib/features/pokemon/domain/repositories/pokemon_repository.dart
@@ -3,5 +3,5 @@ import 'package:poke_api_app/core/error/failure.dart';
 import 'package:poke_api_app/features/pokemon/data/models/pokemon_model.dart';
 
 abstract class PokemonRepostiry {
-  Future<Either<Failure, List<Results>>> getPokemonFromAPI();
+  Future<Either<Failure, List<Results>>> getPokemonFromAPI({int offset = 0});
 }
