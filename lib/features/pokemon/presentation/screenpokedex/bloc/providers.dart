46a4293305c6d19b7f5f70848e61e3edd399100a
scolocho <scolocho@popoyan.com>
2021-04-15 15:24:32 -0600

    dependency injection with riverod

--- lib/features/pokemon/presentation/screenpokedex/bloc/providers.dart
+++ lib/features/pokemon/presentation/screenpokedex/bloc/providers.dart
@@ -1,8 +1,55 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-import 'package:poke_api_app/features/pokemon/presentation/screenpokedex/bloc/pokemon_bloc.dart';
+import '../../../../../core/network/network_info.dart';
+import '../../../data/datasource/pokemon_remote_data_source.dart';
+import '../../../data/repositories/pokemon_repository_impl.dart';
+import '../../../domain/repositories/pokemon_repository.dart';
+import '../../../domain/usecases/get_pokemon.dart';
+import 'pokemon_bloc.dart';
 
-import '../../../../../injection_container.dart';
+//bloc
+final pokedexBloc = ChangeNotifierProvider<PokedexBloc>(
+  (ref) => PokedexBloc(
+    getPokemon: ref.watch(_diUseCase),
+  ),
+);
 
-final pokedexBloc =
-    ChangeNotifierProvider<PokedexBloc>((ref) => sl<PokedexBloc>());
+//use case
+final _diUseCase = Provider<GetPokemon>(
+  (ref) => GetPokemon(
+    ref.watch(_diRepository),
+  ),
+);
+
+//repositories
+final _diRepository = Provider<PokemonRepostiry>(
+  (ref) => PokemonRepositoryImpl(
+    pokemonRemoteDataSource: ref.watch(_diDataSource),
+    networkInfo: ref.watch(_diNetworInfo),
+  ),
+);
+
+//datasource
+final _diDataSource = Provider<PokemonRemoteDataSource>(
+  (ref) => PokemonRemoteDataSourceImpl(
+    client: ref.watch(_diHttp),
+  ),
+);
+
+//!core
+final _diNetworInfo = Provider<NetworkInfo>(
+  (ref) => NetworkInfoImpl(
+    ref.watch(_diDataconection),
+  ),
+);
+
+//External
+final _diDataconection = Provider<DataConnectionChecker>(
+  (ref) => DataConnectionChecker(),
+);
+final _diHttp = Provider<Client>(
+  (ref) => http.Client(),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'injection_container.dart' as di;
 import 'my_app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  runApp(ProviderScope(child: MyApp()));
-}
+void main() => runApp(ProviderScope(child: MyApp()));
